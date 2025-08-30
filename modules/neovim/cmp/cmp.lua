73c423d7b8b40b49cd967ce64d2832a6426d45a7
drainpixie <121581793+drainpixie@users.noreply.github.com>
2025-08-30 14:46:23 +0200

    feat(modules/neovim/cmp): disable when minimal

--- modules/neovim/cmp/cmp.lua
+++ modules/neovim/cmp/cmp.lua
@@ -1,28 +1,30 @@
-local cmp = require("cmp")
+local ok, cmp = pcall(require, "cmp")
 
 vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { undercurl = true, fg = "#993333" })
 vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", { undercurl = true, fg = "#9E7B1C" })
 
-cmp.setup.cmdline({ "/", "?" }, {
-	sources = {
-		{ name = "buffer" },
-	},
-})
+if ok then
+	cmp.setup.cmdline({ "/", "?" }, {
+		sources = {
+			{ name = "buffer" },
+		},
+	})
 
-cmp.setup.filetype("gitcommit", {
-	sources = cmp.config.sources({
-		{ name = "cmp_git" },
-	}, {
-		{ name = "buffer" },
-	}),
-})
+	cmp.setup.filetype("gitcommit", {
+		sources = cmp.config.sources({
+			{ name = "cmp_git" },
+		}, {
+			{ name = "buffer" },
+		}),
+	})
 
-cmp.setup.cmdline(":", {
-	sources = cmp.config.sources({
-		{ name = "path" },
-	}, {
-		{ name = "cmdline" },
-	}),
-})
+	cmp.setup.cmdline(":", {
+		sources = cmp.config.sources({
+			{ name = "path" },
+		}, {
+			{ name = "cmdline" },
+		}),
+	})
 
-require("copilot").setup()
+	require("copilot").setup()
+end
--- modules/neovim/cmp/default.nix
+++ modules/neovim/cmp/default.nix
@@ -5,86 +5,90 @@
   ...
 }: {
   config = lib.mkIf config.my.neovim.enable {
-    programs.nixvim = {
-      extraConfigLua = builtins.readFile ./cmp.lua;
+    programs.nixvim = lib.mkMerge [
+      {
+        extraConfigLua = builtins.readFile ./cmp.lua;
+      }
 
-      plugins =
-        tools.enableMany
-        ["luasnip" "cmp-nvim-lsp" "cmp-buffer" "cmp-path" "cmp_luasnip" "cmp-cmdline" "cmp-emoji" "copilot-cmp"]
-        // {
-          copilot-lua = {
-            enable = true;
-            settings = {
-              suggestion.enabled = false;
-              panel.enabled = false;
-            };
-          };
-
-          lspkind = {
-            enable = true;
-            settings.mode = "symbol";
-          };
-
-          cmp = {
-            enable = true;
-
-            settings = {
-              performance = {
-                debounce = 60;
-                fetchingTimeout = 200;
-                maxViewEntries = 30;
-              };
-
-              autoEnableSources = true;
-              snippet.expand = "luasnip";
-              experimental.ghost_text = true;
-              formatting.fields = ["kind" "abbr" "menu"];
-
-              window = {
-                completion = {
-                  border = "rounded";
-                  winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:Visual";
-                };
-
-                documentation = {
-                  border = "rounded";
-                  winhighlight = "Normal:Normal,FloatBorder:Normal";
-                };
-              };
-
-              sources = [
-                {name = "nvim_lsp";}
-                {name = "emoji";}
-                {
-                  name = "buffer";
-                  option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
-                  keywordLength = 3;
-                }
-                {name = "copilot";}
-                {
-                  name = "path";
-                  keywordLength = 3;
-                }
-                {
-                  name = "luasnip";
-                  keywordLength = 3;
-                }
-              ];
-
-              mapping = {
-                "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-                "<C-j>" = "cmp.mapping.select_next_item()";
-                "<C-k>" = "cmp.mapping.select_prev_item()";
-                "<C-e>" = "cmp.mapping.abort()";
-                "<C-b>" = "cmp.mapping.scroll_docs(-4)";
-                "<C-f>" = "cmp.mapping.scroll_docs(4)";
-                "<C-Space>" = "cmp.mapping.complete()";
-                "<CR>" = "cmp.mapping.confirm({ select = true })";
-                "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
+      (lib.mkIf (!config.my.neovim.minimal) {
+        plugins =
+          tools.enableMany
+          ["luasnip" "cmp-nvim-lsp" "cmp-buffer" "cmp-path" "cmp_luasnip" "cmp-cmdline" "cmp-emoji" "copilot-cmp"]
+          // {
+            copilot-lua = {
+              enable = true;
+              settings = {
+                suggestion.enabled = false;
+                panel.enabled = false;
               };
             };
+
+            lspkind = {
+              enable = true;
+              settings.mode = "symbol";
+            };
+
+            cmp = {
+              enable = true;
+
+              settings = {
+                performance = {
+                  debounce = 60;
+                  fetchingTimeout = 200;
+                  maxViewEntries = 30;
+                };
+
+                autoEnableSources = true;
+                snippet.expand = "luasnip";
+                experimental.ghost_text = true;
+                formatting.fields = ["kind" "abbr" "menu"];
+
+                window = {
+                  completion = {
+                    border = "rounded";
+                    winhighlight = "Normal:Normal,FloatBorder:Normal,CursorLine:Visual";
+                  };
+
+                  documentation = {
+                    border = "rounded";
+                    winhighlight = "Normal:Normal,FloatBorder:Normal";
+                  };
+                };
+
+                sources = [
+                  {name = "nvim_lsp";}
+                  {name = "emoji";}
+                  {
+                    name = "buffer";
+                    option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+                    keywordLength = 3;
+                  }
+                  {name = "copilot";}
+                  {
+                    name = "path";
+                    keywordLength = 3;
+                  }
+                  {
+                    name = "luasnip";
+                    keywordLength = 3;
+                  }
+                ];
+
+                mapping = {
+                  "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+                  "<C-j>" = "cmp.mapping.select_next_item()";
+                  "<C-k>" = "cmp.mapping.select_prev_item()";
+                  "<C-e>" = "cmp.mapping.abort()";
+                  "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+                  "<C-f>" = "cmp.mapping.scroll_docs(4)";
+                  "<C-Space>" = "cmp.mapping.complete()";
+                  "<CR>" = "cmp.mapping.confirm({ select = true })";
+                  "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
+                };
+              };
+            };
           };
-        };
-    };
+      })
+    ];
   };
 }
